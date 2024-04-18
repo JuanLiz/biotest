70c781f55660e3428f86cc460acec4f70b71521c
Diego Lizarazo <juand5455@gmail.com>
2024-04-17 23:37:25 -0500

    fix: Add migration for column name change

---
+++ Biotest/Migrations/20240418043654_ChangeResultsColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Biotest.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeResultsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Result",
+                table: "GeneticTest",
+                newName: "Results");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Results",
+                table: "GeneticTest",
+                newName: "Result");
+        }
+    }
+}
